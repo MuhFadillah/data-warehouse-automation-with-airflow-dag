3b12ad5230e37992739d5cb8eedda45a78c79a0c
Muhammad Fadillah <mfadillah346@gmail.com>
2025-06-02 16:30:45 +0700

    Update run-minio.sh

--- minio/run-minio.sh/run-minio.sh
+++ minio/run-minio.sh/run-minio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export MINIO_ROOT_USER=minioadmin
-export MINIO_ROOT_PASSWORD=minioadmin123
+export MINIO_ROOT_USER=(your username)
+export MINIO_ROOT_PASSWORD=(your password)
 
 ./minio server ./data --console-address ":9001" --address ":9000"
