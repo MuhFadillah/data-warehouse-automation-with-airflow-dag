304351bf4c0f0eb0619cf1d96853a2561638258f
Muhammad Fadillah <mfadillah346@gmail.com>
2025-06-03 09:07:04 +0700

    Update elt_erp_loc_a101.py

--- spark/scripts/elt_erp_loc_a101.py
+++ spark/scripts/elt_erp_loc_a101.py
@@ -6,16 +6,16 @@ spark = SparkSession.builder \
     .appName("ELT_ERP_LOC_A101") \
     .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.2,com.amazonaws:aws-java-sdk-bundle:1.11.901") \
     .config("spark.hadoop.fs.s3a.endpoint", "http://localhost:9000") \
-    .config("spark.hadoop.fs.s3a.access.key", "minioadmin") \
-    .config("spark.hadoop.fs.s3a.secret.key", "minioadmin123") \
+    .config("spark.hadoop.fs.s3a.access.key", "your username") \
+    .config("spark.hadoop.fs.s3a.secret.key", "your password") \
     .config("spark.hadoop.fs.s3a.path.style.access", "true") \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
     .getOrCreate()
 
-# Baca file dari MinIO (raw layer)
+# Read file from MinIO (raw layer)
 df = spark.read.option("header", True).csv("s3a://raw/erp/LOC_A101.csv")
 
-# Transformasi
+# Transformation
 df_transformed = df.select(
     expr("REPLACE(cid, '-', '')").alias("cid"),
     when(trim(col("cntry")) == "DE", "Germany")
@@ -24,7 +24,7 @@ df_transformed = df.select(
     .otherwise(trim(col("cntry"))).alias("cntry")
 )
 
-# Simpan hasil ke MinIO (clean layer)
+# Save results to MinIO (clean layer)
 df_transformed.write \
     .mode("overwrite") \
     .option("header", True) \
